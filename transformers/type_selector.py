b4471fd0eb0a16545f33fc195ebb1f324a703175
Tao <taohu88@gmail.com>
2019-03-29 10:50:37 -0700

    add some transformers

---
+++ transformers/type_selector.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class TypeSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, dtype):
+        self.dtype = dtype
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        assert isinstance(X, pd.DataFrame)
+        return X.select_dtypes(include=[self.dtype])
+
+
+class StringIndexer(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        assert isinstance(X, pd.DataFrame)
+        return X.apply(lambda s: s.cat.codes.replace(
+            {-1: len(s.cat.categories)}
+        ))
+
