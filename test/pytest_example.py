30da3e57aaa469d896a34f2e706d34fe9df4ad99
Tao Hu <taohu88@gmail.com>
2019-05-06 20:40:33 -0700

    add pytest.ini

--- test/pytest_example.py
+++ test/pytest_example.py
@@ -7,3 +7,11 @@ def test_equal():
     expected = [1, 2, 3]
     print(f"actual is {actual}")
     assert expected == actual
+
+
+class TestSomeStuff():
+    def test_three(selfs):
+        assert {1, 2, 2} == {1, 2}
+
+    def test_four(self):
+        assert [1, 2] == [1, 2]
